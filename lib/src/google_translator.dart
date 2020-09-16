b55b59c9c7286410eec8aad30ff0498bb11a6c60
Timur Kim <allrighty1809@gmail.com>
2020-09-16 11:57:11 +0600

    Fully extended package commit
    Change-Id: Ie8cc37792e2ae39c066850bfca8aebb2d2dcf29e

--- lib/src/google_translator.dart
+++ lib/src/google_translator.dart
@@ -69,12 +69,202 @@ class GoogleTranslator {
     }
 
     final translated = sb.toString();
+    final alternateTranslated = await _getAltTranslation(sourceText, from, to);
+    final definition = await _getDefinition(sourceText, from, to);
+    final synonyms = await _getSynonyms(sourceText, from, to);
+    final examples = await _getExamples(sourceText, from, to);
     return _Translation(
       translated,
+      alternateTranslated,
+      definition,
+      synonyms,
+      examples,
       source: sourceText,
       sourceLanguage: _languageList[from],
       targetLanguage: _languageList[to],
     );
+  }
+
+  // Get alt. translation
+  Future<List<String>> _getAltTranslation(
+    String sourceText,
+    String from,
+    String to,
+  ) async {
+    final parameters = {
+      'client': client == ClientType.siteGT ? 't' : 'gtx',
+      'sl': from,
+      'tl': to,
+      'hl': to,
+      'dt': 'at',
+      'ie': 'UTF-8',
+      'oe': 'UTF-8',
+      'otf': '1',
+      'ssel': '0',
+      'tsel': '0',
+      'kc': '7',
+      'tk': _tokenProvider.generateToken(sourceText),
+      'q': sourceText
+    };
+
+    var url = Uri.https(_baseUrl, _path, parameters);
+    final data = await http.get(url);
+
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+    }
+
+    final jsonData = jsonDecode(data.body);
+    if (jsonData[5] != null) {
+      final List<String> tList = [];
+
+      final List correctData = jsonData[5][0][2];
+
+      for (int i = 0; i < correctData.length; i++) {
+        tList.add(jsonData[5][0][2][i][0]);
+      }
+      return tList;
+    } else {
+      return null;
+    }
+  }
+
+  // Get definition of text
+  Future<Map<String, List<String>>> _getDefinition(
+    String sourceText,
+    String from,
+    String to,
+  ) async {
+    final parameters = {
+      'client': client == ClientType.siteGT ? 't' : 'gtx',
+      'sl': from,
+      'tl': to,
+      'hl': to,
+      'dt': 'md',
+      'ie': 'UTF-8',
+      'oe': 'UTF-8',
+      'otf': '1',
+      'ssel': '0',
+      'tsel': '0',
+      'kc': '7',
+      'tk': _tokenProvider.generateToken(sourceText),
+      'q': sourceText
+    };
+
+    var url = Uri.https(_baseUrl, _path, parameters);
+    final data = await http.get(url);
+
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+    }
+
+    final jsonData = jsonDecode(data.body);
+
+    Map<String, List<String>> definitionsMap = {};
+    if (jsonData.length < 12)
+      return null;
+    else {
+      if (jsonData[12] != null) {
+        final List list = jsonData[12];
+        for (int i = 0; i < list.length; i++) {
+          String title = jsonData[12][i][0];
+          List<String> definitions = [];
+          List inList = jsonData[12][i][1];
+          for (int n = 0; n < inList.length; n++)
+            definitions.add(jsonData[12][i][1][n][0]);
+          definitionsMap[title] = definitions;
+        }
+        return definitionsMap;
+      } else
+        return null;
+    }
+  }
+
+  // Get synonyms
+  Future<List> _getSynonyms(
+    String sourceText,
+    String from,
+    String to,
+  ) async {
+    final parameters = {
+      'client': client == ClientType.siteGT ? 't' : 'gtx',
+      'sl': from,
+      'tl': to,
+      'hl': to,
+      'dt': 'ss',
+      'ie': 'UTF-8',
+      'oe': 'UTF-8',
+      'otf': '1',
+      'ssel': '0',
+      'tsel': '0',
+      'kc': '7',
+      'tk': _tokenProvider.generateToken(sourceText),
+      'q': sourceText
+    };
+
+    var url = Uri.https(_baseUrl, _path, parameters);
+    final data = await http.get(url);
+
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+    }
+
+    final jsonData = jsonDecode(data.body);
+
+    List<String> filteredData = [];
+    if (jsonData.length < 11)
+      return null;
+    else {
+      for (int i = 0; i < jsonData[11].length; i++)
+        for (int n = 0; n < jsonData[11][i][1].length; n++)
+          for (int t = 0; t < jsonData[11][i][1][n][0].length; t++)
+            filteredData.add(jsonData[11][i][1][n][0][t]);
+    }
+    return filteredData;
+  }
+
+  Future<List> _getExamples(
+    String sourceText,
+    String from,
+    String to,
+  ) async {
+    final parameters = {
+      'client': client == ClientType.siteGT ? 't' : 'gtx',
+      'sl': from,
+      'tl': to,
+      'hl': to,
+      'dt': 'ex',
+      'ie': 'UTF-8',
+      'oe': 'UTF-8',
+      'otf': '1',
+      'ssel': '0',
+      'tsel': '0',
+      'kc': '7',
+      'tk': _tokenProvider.generateToken(sourceText),
+      'q': sourceText
+    };
+
+    var url = Uri.https(_baseUrl, _path, parameters);
+    final data = await http.get(url);
+
+    if (data.statusCode != 200) {
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+    }
+
+    final jsonData = jsonDecode(data.body);
+
+    if (jsonData.length < 13)
+      return null;
+    else {
+      List<String> list = [];
+      for (int i = 0; i < jsonData[13][0].length; i++) {
+        String string = jsonData[13][0][i][0];
+        String formattedString =
+            string.replaceAll('<b>$sourceText</b>', '$sourceText');
+        list.add(formattedString);
+      }
+      return list;
+    }
   }
 
   /// Translates and prints directly
--- lib/src/model/translation.dart
+++ lib/src/model/translation.dart
@@ -3,12 +3,24 @@ part of google_transl;
 /// Translation returned from GoogleTranslator.translate method, containing the translated text, the source text, the translated language and the source language
 abstract class Translation {
   final String text;
+  final List<String> alternative;
+  final Map<String, List<String>> definition;
+  final List<String> synonyms;
+  final List<String> examples;
   final String source;
   final Language targetLanguage;
   final Language sourceLanguage;
 
   Translation._(
-      this.text, this.source, this.sourceLanguage, this.targetLanguage);
+    this.text,
+    this.source,
+    this.sourceLanguage,
+    this.targetLanguage,
+    this.alternative,
+    this.definition,
+    this.synonyms,
+    this.examples,
+  );
 
   String operator +(other);
 
@@ -18,16 +30,33 @@ abstract class Translation {
 
 class _Translation extends Translation {
   final String text;
+  final List<String> alternative;
+  final Map<String, List<String>> definition;
+  final List<String> synonyms;
+  final List<String> examples;
   final String source;
   final Language sourceLanguage;
   final Language targetLanguage;
 
   _Translation(
-    this.text, {
+    this.text,
+    this.alternative,
+    this.definition,
+    this.synonyms,
+    this.examples, {
     this.sourceLanguage,
     this.targetLanguage,
     this.source,
-  }) : super._(text, source, sourceLanguage, targetLanguage);
+  }) : super._(
+          text,
+          source,
+          sourceLanguage,
+          targetLanguage,
+          alternative,
+          definition,
+          synonyms,
+          examples,
+        );
 
   String operator +(other) => this.toString() + other.toString();
 }
