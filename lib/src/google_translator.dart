24ef84ae84a525c4aef829c730bd55e97839eeb8
Timur Kim <allrighty1809@gmail.com>
2020-09-16 11:57:11 +0600

    SHLANG-34 Extend the library to get all translation cases, synonymous and examples
    Change-Id: Ie8cc37792e2ae39c066850bfca8aebb2d2dcf29e

--- lib/src/google_translator.dart
+++ lib/src/google_translator.dart
@@ -7,6 +7,10 @@ import './tokens/google_token_gen.dart';
 import './langs/language.dart';
 
 part './model/translation.dart';
+part './model/definition.dart';
+part './model/synonym.dart';
+part './model/example.dart';
+part './model/alternative_translation.dart';
 
 ///
 /// This library is a Dart implementation of Google Translate API
@@ -22,37 +26,9 @@ class GoogleTranslator {
 
   GoogleTranslator({this.client = ClientType.siteGT});
 
-  /// Translates texts from specified language to another
   Future<Translation> translate(String sourceText,
       {String from = 'auto', String to = 'en'}) async {
-    for (var each in [from, to]) {
-      if (!LanguageList.contains(each)) {
-        throw LanguageNotSupportedException(each);
-      }
-    }
-
-    final parameters = {
-      'client': client == ClientType.siteGT ? 't' : 'gtx',
-      'sl': from,
-      'tl': to,
-      'hl': to,
-      'dt': 't',
-      'ie': 'UTF-8',
-      'oe': 'UTF-8',
-      'otf': '1',
-      'ssel': '0',
-      'tsel': '0',
-      'kc': '7',
-      'tk': _tokenProvider.generateToken(sourceText),
-      'q': sourceText
-    };
-
-    var url = Uri.https(_baseUrl, _path, parameters);
-    final data = await http.get(url);
-
-    if (data.statusCode != 200) {
-      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
-    }
+    var data = await _getData(sourceText, from: from, to: to, dataType: 't');
 
     final jsonData = jsonDecode(data.body);
     final sb = StringBuffer();
@@ -60,14 +36,6 @@ class GoogleTranslator {
     for (var c = 0; c < jsonData[0].length; c++) {
       sb.write(jsonData[0][c][0]);
     }
-
-    if (from == 'auto' && from != to) {
-      from = jsonData[2] ?? from;
-      if (from == to) {
-        from = 'auto';
-      }
-    }
-
     final translated = sb.toString();
     return _Translation(
       translated,
@@ -75,6 +43,114 @@ class GoogleTranslator {
       sourceLanguage: _languageList[from],
       targetLanguage: _languageList[to],
     );
+  }
+
+  Future<AlternativeTranslation> getAltTranslation(String sourceText,
+      {String from = 'auto', String to = 'en'}) async {
+    var data = await _getData(sourceText, from: from, to: to, dataType: 'at');
+    final jsonData = jsonDecode(data.body);
+
+    final List<String> words = [];
+
+    if (jsonData[5] != null) {
+      final List correctData = jsonData[5][0][2];
+      for (final i in correctData) {
+        words.add(i[0]);
+      }
+      return AlternativeTranslation(
+        words,
+        source: sourceText,
+        sourceLanguage: _languageList[from],
+        targetLanguage: _languageList[to],
+      );
+    } else {
+      print('There are no alternative translations');
+      return null;
+    }
+  }
+
+  Future<Definition> getDefinition(String sourceText,
+      {String from = 'auto', String to = 'en'}) async {
+    final data = await _getData(sourceText, from: from, to: to, dataType: 'md');
+
+    final jsonData = jsonDecode(data.body);
+
+    Map<String, List<String>> definitionsMap = {};
+    if (jsonData.length >= 12 && jsonData[12] != null) {
+      final List list = jsonData[12];
+      for (final i in list) {
+        String title = i[0];
+        List<String> definitions = [];
+        List inList = i[1];
+        for (final n in inList) {
+          definitions.add(n[0]);
+        }
+        definitionsMap[title] = definitions;
+      }
+      return Definition(
+        definitionsMap,
+        source: sourceText,
+        sourceLanguage: _languageList[from],
+        targetLanguage: _languageList[to],
+      );
+    } else {
+      print('There are no definitions');
+      return null;
+    }
+  }
+
+  Future<Synonym> getSynonyms(String sourceText,
+      {String from = 'auto', String to = 'en'}) async {
+    final data = await _getData(sourceText, from: from, to: to, dataType: 'ss');
+
+    final jsonData = jsonDecode(data.body);
+
+    List<String> filteredData = [];
+    if (jsonData.length < 11) {
+      print('There are no synonyms');
+      return null;
+    } else {
+      for (final i in jsonData[11]) {
+        for (final n in i[1]) {
+          for (final t in n[0]) {
+            filteredData.add(t);
+          }
+        }
+      }
+      filteredData.removeRange(5, filteredData.length);
+    }
+    return Synonym(
+      filteredData,
+      source: sourceText,
+      targetLanguage: _languageList[to],
+      sourceLanguage: _languageList[from],
+    );
+  }
+
+  Future<Example> getExamples(String sourceText,
+      {String from = 'auto', String to = 'en'}) async {
+    final data = await _getData(sourceText, from: from, to: to, dataType: 'ex');
+
+    final jsonData = jsonDecode(data.body);
+
+    if (jsonData.length < 13) {
+      print('There are no examples');
+      return null;
+    } else {
+      List<String> list = [];
+      for (final i in jsonData[13][0]) {
+        String string = i[0];
+        String formattedString =
+            string.replaceAll('<b>$sourceText</b>', '$sourceText');
+        list.add(formattedString);
+      }
+      return Example(
+        list,
+        source: sourceText,
+        sourceLanguage: _languageList[from],
+        targetLanguage: _languageList[to],
+      );
+    }
   }
 
   /// Translates and prints directly
@@ -85,6 +161,39 @@ class GoogleTranslator {
 
   /// Sets base URL for countries that default URL doesn't work
   void set baseUrl(String url) => _baseUrl = url;
+
+  Future<http.Response> _getData(String sourceText,
+      {String from, String to, String dataType}) async {
+    for (var each in [from, to]) {
+      if (!LanguageList.contains(each)) {
+        throw LanguageNotSupportedException(each);
+      }
+    }
+
+    final Map<String, String> parameters = {
+      'client': client == ClientType.siteGT ? 't' : 'gtx',
+      'sl': from,
+      'tl': to,
+      'hl': to,
+      'dt': dataType,
+      'ie': 'UTF-8',
+      'oe': 'UTF-8',
+      'otf': '1',
+      'ssel': '0',
+      'tsel': '0',
+      'kc': '7',
+      'tk': _tokenProvider.generateToken(sourceText),
+      'q': sourceText
+    };
+
+    final url = Uri.https(_baseUrl, _path, parameters);
+    final data = await http.get(url);
+
+    if (data.statusCode != 200)
+      throw http.ClientException('Error ${data.statusCode}: ${data.body}', url);
+
+    return data;
+  }
 }
 
 enum ClientType {
